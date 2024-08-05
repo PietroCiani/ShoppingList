4b6988dd6ec0d9ad7b0fb8f3655572a43b44d6d2
deltaplanoo <pietro.ciani01@gmail.com>
2024-08-05 20:38:25 +0200

    new example

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include "List.h"
 #include "Prod.h"
+#include "List.h"
 
 int main(){
-    List casa;
-    //Prod latte("Latte", casa, 1);
+    List list1("Casa");
+    List list2("Grigliata");
+
+    list1.addProd(Prod("Pane", 2));
+    list1.addProd(Prod("Latte"));
+    list1.addProd(Prod("Uova", 6));
+
+    list2.addProd(Prod("Salsicce", 10));
+    list2.addProd(Prod("Hamburger", 5));
+    list2.addProd(Prod("Carbonella"));
+
+    list1.printList();
+    list2.printList();
+
     return 0;
 };
