fe5ac08edd457aba03ce5a775eb1319fd67a2849
deltaplanoo <pietro.ciani01@gmail.com>
2024-08-13 15:54:56 +0200

    [new] add
    add prod to list
    capitalize prod

--- main.cpp
+++ main.cpp
@@ -3,11 +3,21 @@
 #include "List.h"
 #include "Button.h"
 
+std::string capitalizeFirstLetter(const std::string& str) {
+    if (str.empty()) {
+        return str;
+    }
+    std::string capitalized = str;
+    capitalized[0] = std::toupper(capitalized[0]);
+    std::transform(capitalized.begin() + 1, capitalized.end(), capitalized.begin() + 1, ::tolower);
+    return capitalized;
+}
+
 int main(){
     List shoppingList("Casa");
-    shoppingList.addProd("Pane");
-    shoppingList.addProd("Latte", 2);
     shoppingList.addProd("Uova", 6);
+    shoppingList.addProd("Latte", 2);
+    shoppingList.addProd("Pane");
 
     // vectors to be drawn
     std::vector<std::unique_ptr<sf::Drawable>> drawables;
@@ -25,8 +35,36 @@ int main(){
         return -1;
     }
 
+    std::string inputText;
+    float yPos = 0.85;
+    auto textField = std::make_unique<sf::Text>();
+    textField->setFont(font);
+    textField->setCharacterSize(14);
+    textField->setFillColor(fg);
+    sf::FloatRect textRect = textField->getLocalBounds();
+    textField->setOrigin(textRect.left + textRect.width / 2.f, textRect.top + textRect.height / 2.f);
+    textField->setPosition(w.x / 2.f, w.y *yPos);
+    /*
+    Button addToList("Aggiungi", {w.x * 0.65f, w.y * yPos}, [&shoppingList, &inputText](){
+        if (!inputText.empty()){
+            int prodIndex = shoppingList.searchProdIndex(inputText);
+            std::cout << "Cerco " << shoppingList.getItems(prodIndex).getName() << " nella lista" << std::endl;
+            if (prodIndex == -1){
+                std::cout << shoppingList.getItems(prodIndex).getName() << " non c'è -> aggiungo" << std::endl;
+                shoppingList.addProd(inputText);
+                inputText.clear();
+            } else {
+                std::cout << shoppingList.getItems(prodIndex).getName() << " è gia nella lista (+1)" << std::endl;
+                shoppingList.getItems(prodIndex).setAmount(shoppingList.getItems(prodIndex).getAmount()+1, true);
+            }
+        }
+    }, font, {70,30});
+    buttons.push_back(std::make_unique<Button>(addToList));
+    */
     while (window.isOpen()) {
         sf::Event event{};
+        //TODO: show textField
+        //drawables.push_back(std::move(textField));
         window.clear(bg);
         /*
         Button test("+", {w.x * 0.5f, w.y * 0.5f},[&w](){std::cout << "window is " << w.x << " x " << w.y << std::endl;}, font);
@@ -81,6 +119,30 @@ int main(){
                 view.setCenter(float(event.size.width) / 2.f, float(event.size.height) / 2.f);
                 window.setView(view);
                 w = {static_cast<float>(event.size.width), static_cast<float>(event.size.height)};
+            }
+            if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode == '\b') {
+                    if (!inputText.empty()) {
+                        inputText.pop_back();
+                    }
+                } else if (event.text.unicode == '\r') {
+                    if (!inputText.empty()){
+                        int prodIndex = shoppingList.searchProdIndex(inputText);
+                        std::cout << "Cerco " << inputText << " nella lista" << std::endl;
+                        if (prodIndex == -1) {
+                            std::cout << inputText << " non c'è -> aggiungo" << std::endl;
+                            shoppingList.addProd(capitalizeFirstLetter(inputText));
+                            inputText.clear();
+                        } else {
+                            std::cout << shoppingList.getItems(prodIndex).getName() << " è gia nella lista (+1)" << std::endl;
+                            shoppingList.getItems(prodIndex).setAmount(shoppingList.getItems(prodIndex).getAmount()+1, true);
+                            inputText.clear();
+                        }
+                    }
+                } else {
+                    inputText += static_cast<char>(event.text.unicode);
+                }
+                textField->setString(inputText);
             }
         }
 
