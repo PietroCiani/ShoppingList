020843b812fceec4474687c1af1d0e9cf390da81
deltaplanoo <pietro.ciani01@gmail.com>
2024-08-30 22:28:31 +0200

    [fix] not good

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/delta/Documents/Uni/Lab Programmazione/ShoppingList
-# Build directory: /home/delta/Documents/Uni/Lab Programmazione/ShoppingList/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("_deps/googletest-build")
